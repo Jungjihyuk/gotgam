967019900df93cb97a5d9ddeeb0919a0317b3645
YujinHyeon <>
2019-09-14 01:00:47 +0900

    Add Bootstrap & Container complete

--- src/components/container.vue
+++ src/components/container.vue
@@ -5,6 +5,7 @@
 		<Section1></Section1>
 		<Section2></Section2>
 		<Section3></Section3>
+		<Section4></Section4>
 	  </div>	
   </div>
 </template>
@@ -14,13 +15,15 @@ import HomeHeader from './homeHeader.vue';
 import Section1 from './section1.vue';
 import Section2 from './section2.vue';
 import Section3 from './section3.vue';
+import Section4 from './section4.vue';
 
 export default {
 	components: {
 		HomeHeader,
 		Section1,
 		Section2,
-		Section3
+		Section3,
+		Section4
 	}
 }
 </script>
